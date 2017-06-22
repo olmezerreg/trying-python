7112030a20d8e0942c1ecb28e3fa9f553af88ce0
Omar Lamine <ol.mezerreg@gmail.com>
2017-06-22 16:16:23 -0100

    calculatins arai of rect

---
+++ alhsab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#Filename; alhab
+
+#the arai of rect
+def arai_rect():
+	lenght=input("plaes enter your lenght : ");
+	wight =input("plaes enter your wight  : ");
+	area  = wight * lenght
+	print "the area  is ",area ;	
+
+arai_rect()

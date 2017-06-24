a6c8e61e3bce10d90b8d6f2680347994f8f5fe04
Omar Lamine <ol.mezerreg@gmail.com>
2017-06-24 12:19:19 -0100

    calculations area of square

--- alhsab.py
+++ alhsab.py
@@ -1,11 +1,50 @@
 #!/usr/bin/python
-#Filename; alhab
+#Filename; alhsab
 
-#the arai of rect
-def arai_rect():
+#======================================================================================================#
+                                                                                                       
+                                                                                                       
+                                                                                                       
+#the arai of rect                                                                                      
+def area_rect():
 	lenght=input("plaes enter your lenght : ");
 	wight =input("plaes enter your wight  : ");
 	area  = wight * lenght
 	print "the area  is ",area ;	
+area_rect()
 
-arai_rect()
+
+
+#the arai of square
+def area_square():
+ side=input("plaes enter your side : ");
+ area=side**2
+ print "the area  is ",area ;
+	
+area_square()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
